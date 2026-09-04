9f9a1d33f7d786bfee2cbfca2780715499595170
redkeke96-collab <redkeke96@gmail.com>
2026-09-04 17:07:06 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "dtro-clean-offline-v13-touch-fix";
+const CACHE = "dtro-clean-offline-v14-30questions";
 const CORE = ["./index.html","./manifest.json","./tagu.png"];
 
 self.addEventListener("install", event => {
